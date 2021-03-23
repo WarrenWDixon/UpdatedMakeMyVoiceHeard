c1fa221fce9d9b4b8ffd952a3da81b320955a1b3
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-03-22 22:28:48 -0500

    parse individual officials

--- app/src/main/java/com/example/android/makemyvoiceheard/JsonUtil.java
+++ app/src/main/java/com/example/android/makemyvoiceheard/JsonUtil.java
@@ -12,6 +12,10 @@ class JsonUtil {
         JSONObject jOBJ = null;
         JSONArray jsonOffices = null;
         JSONArray jsonOfficials = null;
+        JSONObject senator1 = null;
+        JSONObject senator2 = null;
+        JSONObject congressman = null;
+
         try {
             jOBJ = new JSONObject(json);
             Log.d("WWD", "jOBJ is " + jOBJ.toString());
@@ -46,6 +50,30 @@ class JsonUtil {
             Log.d("WWD", "officials are " + jsonOfficials);
         } catch (JSONException e) {
             Log.d("WWD", "error parsing officials");
+            e.printStackTrace();
+        }
+        Log.d("WWD", "the officals arrays size is " + jsonOfficials.length());
+        try {
+            senator1 = jsonOfficials.getJSONObject(0);
+            Log.d("WWD", "senator1 is " + senator1);
+        } catch (JSONException e) {
+            Log.d("WWD", "error parsing senator1");
+            e.printStackTrace();
+        }
+
+        try {
+            senator2 = jsonOfficials.getJSONObject(1);
+            Log.d("WWD", "senator2 is " + senator2);
+        } catch (JSONException e) {
+            Log.d("WWD", "error parsing senator2");
+            e.printStackTrace();
+        }
+
+        try {
+            congressman = jsonOfficials.getJSONObject(2);
+            Log.d("WWD", "congressman is " + congressman);
+        } catch (JSONException e) {
+            Log.d("WWD", "error parsing congressman");
             e.printStackTrace();
         }
 
