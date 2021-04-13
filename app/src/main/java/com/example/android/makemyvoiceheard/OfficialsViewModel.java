4bc227362a0bab9ba4848baed2407a0311ffd4be
Warren Dixon <Warren.Dixon@e-hps.com>
2021-04-12 22:56:25 -0500

    added view model

---
+++ app/src/main/java/com/example/android/makemyvoiceheard/OfficialsViewModel.java
@@ -0,0 +1,38 @@
+package com.example.android.makemyvoiceheard;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.List;
+
+public class OfficialsViewModel extends AndroidViewModel {
+
+    private OfficialsRepository mRepository;
+
+    // private LiveData member variable to cache list of words
+    private LiveData<List<Officials>> mAllOfficials;
+
+    /**
+     * Creates a {@code AndroidViewModelFactory}
+     *
+     * @param application an application to pass in {@link AndroidViewModel}
+     */
+    public OfficialsViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new OfficialsRepository(application);
+        mAllOfficials = mRepository.getAllOfficials();
+    }
+
+    // add a "getter" method that gets all the words, hides implementation from UI
+    LiveData<List<Officials>> getAllMovies() { return mAllOfficials;}
+
+    // create wrapper insert() method that calls Repository's insert() method,
+    // implementation of insert() is hidden from UI
+    public void insert(Officials officials) {
+        mRepository.insert(officials);
+    }
+}
